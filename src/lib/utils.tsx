189f2e9b4cc7c94f9de7ca54d6762e2b5f3cbf29
Tharki-God <alokender018@gmail.com>
2024-02-21 22:14:45 +0530

    hehe

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -41,6 +41,7 @@ export const getElementHex = (
 export const getImageDimensions = (url: string): Promise<{ height: number; width: number }> =>
   new Promise<{ width: number; height: number }>((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = "Anonymous";
     img.onload = () => {
       const canvas = document.createElement("canvas") as HTMLCanvasElement;
       const ctx = canvas.getContext("2d");
