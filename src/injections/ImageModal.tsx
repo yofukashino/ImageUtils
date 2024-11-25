755ce246998bb200604ee385925b8254330eb301
yofukashino <alokender018@gmail.com>
2024-11-26 04:59:43 +0530

    fix video crash

--- src/injections/ImageModal.tsx
+++ src/injections/ImageModal.tsx
@@ -9,7 +9,7 @@ export default async (): Promise<void> => {
   const ImageModalModule = await Modules.ImageModalModulePromise;
   const ImageModal = webpack.getFunctionKeyBySource(ImageModalModule, ".zoomedMediaFitWrapper");
 
-  PluginInjector.after(ImageModalModule, ImageModal, (_args, res: Types.ReactTree) => {
+  PluginInjector.after(ImageModalModule, ImageModal, ([props], res: Types.ReactTree) => {
     const container = Utils.findInReactTree(
       res,
       (c: Types.ReactTree) =>
@@ -20,7 +20,17 @@ export default async (): Promise<void> => {
       (c: Types.ReactTree) =>
         c?.type?.toString?.()?.includes?.("Types.ATTACHMENT") && c?.props?.src,
     ) as Types.ReactTree;
-    if (!Image || !container) return res;
+
+    const isImage =
+      props?.items?.find?.(
+        (i) =>
+          i.original === Image.props.src ||
+          i.url === Image.props.src ||
+          i.proxyUrl === Image.props.src,
+      )?.type !== "VIDEO";
+
+    if (!isImage || !Image || !container) return res;
+
     container.props.children = (
       <ImageDetails src={Image.props.src} children={container.props.children} />
     );
