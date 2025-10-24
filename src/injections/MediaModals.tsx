1e0d0c4f57f2848605794471025666de0172f21f
yofukashino <alokender018@gmail.com>
2025-10-24 16:45:06 +0530

    chore: return

--- src/injections/MediaModals.tsx
+++ src/injections/MediaModals.tsx
@@ -13,7 +13,6 @@ PluginInjector.after(
   Image.prototype,
   "render",
   (_args, res, instance: { props: ImageProps; state: ImageState }) => {
-    return res;
     if (!res?.props || res?.props?.zoomable || res?.props?.alt === "Video") return res;
     const originalChilden = res.props.children ?? ImageComponent;
     res.props.id = "image-utils-modal";
